5d16209c1822b5b3da6c604ea6f9942ebc3746a1
Justin Bean <beanjustinr@gmail.com>
2015-10-05 08:36:42 -0400

    non-functional count method

--- board.rb
+++ board.rb
@@ -30,10 +30,37 @@ attr_reader :size
   end
 
   def living_neighbors(x,y)
-    sum = 0
-    @board.each do |x|
-      @board.each do |y|
+    cell_count = 0
+    x = [-1, 0, 1]
+    y = [-1, 0, 1]
 
+    x.each do |r|
+      y.each do |c|
+        unless c == 0 && r == 0
+          if @board[(x+r)][(y+c)] == 1
+            cell_count += 1
+          end
+        end
+      end
+    end
   end
 
 end
+
+#
+# def neighbors cell
+#
+#   neighbors = []
+#   x = cell.x
+#   y = cell.y
+#   (x-1..x+1).each do |rows|
+#     (y-1..y+1).each do |columns|
+#       next if rows == x && columns == y
+#       if rows >= 0 && columns >= 0
+#       neighbors.push ([rows,columns])
+#     end
+#       end
+#     end
+#     neighbors
+#   end
+# end
